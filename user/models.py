ac5dadbf98574c9a87e25b47c851710becd67d75
sunil-dinday <sunil.dinday@gmail.com>
2017-11-05 16:32:23 +0530

    update

--- user/models.py
+++ user/models.py
@@ -18,6 +18,9 @@ class Documents(models.Model):
 	title=models.CharField(max_length=255,blank=False)
 	abstract=models.TextField(max_length=200,blank=False)
 	document=models.FileField(upload_to='document')
+	downloads=models.IntegerField(default=0)
+	created_on=models.DateField(auto_now=True)
+	
 	CSE='Computer Science and Engineering'
 	ECE='Electronic and Communincation'
 	EEE='Electrical and Electronic Engineering'
@@ -37,6 +40,7 @@ class Documents(models.Model):
 		(pr,'PRIVATE'),
 		(pu,'PUBLIC'),
 		)
+	
 	visibilty=models.CharField(choices=visible,default=pu,max_length=200)
 	author=models.ForeignKey(Author,on_delete=models.CASCADE,null=False)
 	key=models.ForeignKey(KeyWord,on_delete=models.CASCADE,null=True,default='')
